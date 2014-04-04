6b63a5387e4405a2340476e55f45a8db1fcd4a8a
Naor Ye <naorye@gmail.com>
2014-04-04 03:21:00 +0300

    - Remove delicous

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -124,14 +124,6 @@
 
 })(jQuery);
 
-function renderDeliciousLinks(items) {
-    var output = "<ul>";
-    for (var i = 0, l = items.length; i < l; i++) {
-        output += '<li><a href="' + items[i].u + '" title="Tags: ' + (items[i].t == "" ? "" : items[i].t.join(', ')) + '">' + items[i].d + '</a></li>';
-    }
-    output += "</ul>";
-    $('#delicious').html(output);
-}
 
 // iOS scaling bug fix
 // Rewritten version
